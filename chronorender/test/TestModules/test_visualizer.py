94a9b09197b334bf2c116467d4408454a4d65354
gorb <abartholome2@gmail.com>
2013-01-13 23:01:41 -0600

    fixed a bug from copy pasting tests

--- chronorender/test/TestModules/test_visualizer.py
+++ chronorender/test/TestModules/test_visualizer.py
@@ -5,10 +5,11 @@ from visualizer import Visualizer
 class VisualizerTestCase(unittest.TestCase):
     def setUp(self):
         self._cr = ChronoRender()
+        self._factory = self._cr._factories[Visualizer.getTypeName()]
 
     def tearDown(self):
         del self._cr
 
     def test_createAndRunRenderJob(self):
-        create = self._factory.build(Simulation.getTypeName())
+        create = self._factory.build(Visualizer.getTypeName())
         self.assertTrue(create != None)
