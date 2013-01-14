fb49e9038d332725306d7d7e5a33f2dd0fb7545a
gorb <abartholome2@gmail.com>
2013-01-13 22:18:01 -0600

    added logging to renders

--- chronorender/rndr_job.py
+++ chronorender/rndr_job.py
@@ -19,22 +19,31 @@ class RndrJob():
         self._frames        = self._rndrdoc.getFrameRange()
         self._outputpath    = self._rndrdoc.getOutputFileDir()
         self._outputdirs    = ['OUTPUT', 'SHADERS', 'SCRIPTS', 'ASSETS', 'LOG']
-        self._logfile       = os.path.join(self._outputpath, 'log_' + str(self._timecreated) + '.txt')
+
+        self._logfilename   = os.path.join(os.path.join(self._outputpath, 'LOG'), 'log_' + str(self._timecreated) + '.txt')
+        self._logfile       = None
 
         self._renderer      = None
 
     def run(self):
         self._createOutDirs()
+        self._openLogFile()
         self._startRenderer()
         for i in range(self._frames[0], self._frames[1]+1):
             name = self._rndrdoc.getOutputDataFilePath(i)
             self._writeToLog('starting render ' + name + ' at: ' + str(datetime.datetime.now()))
             self._rndrdoc.render(self._renderer, framenumber=i)
             self._writeToLog('finished render ' + name + ' at: ' + str(datetime.datetime.now()))
+        self._closeLogFile()
 
     def _writeToLog(self, content):
-        print content
-        return
+        self._logfile.write(content+'\n')
+
+    def _openLogFile(self):
+        self._logfile = open(self._logfilename, 'a')
+
+    def _closeLogFile(self):
+        self._logfile.close()
 
     def _startRenderer(self, outstream=''):
         self._writeToLog('starting renderer')
