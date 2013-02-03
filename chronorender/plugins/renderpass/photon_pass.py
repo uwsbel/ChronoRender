98ff1c9d13c5e20ad17432fb991e3477d987bc6f
aaron <abartholome2@wisc.edu>
2013-02-03 12:49:09 -0600

    added a photon_pass plugin

---
+++ chronorender/plugins/renderpass/photon_pass.py
@@ -0,0 +1,48 @@
+import os.path 
+from chronorender.renderpass.renderpass import RenderPassException
+from chronorender.renderpass.raytrace_pass import RayTracePass
+import chronorender.shader as cs
+
+class PhotonPass(RayTracePass):
+
+    @staticmethod
+    def getTypeName():
+        return "envlight"
+
+    def __init__(self, *args, **kwargs):
+        super(PhotonPass,self).__init__(*args, **kwargs)
+
+        self.env = None
+        self.envmap = self.getVar('envmap', kwargs)
+
+        if 'factories' in kwargs:
+            self.env = cs.Shader(name='envlight.sl', factories=kwargs['factories'])
+            self.lighting.append(self.env)
+
+    def _initMembersDict(self):
+        super(PhotonPass, self)._initMembersDict()
+
+        self._members['envmap']   = [str, '']
+
+    def resolveAssets(self, assetman):
+        # resolve envmap texture
+        if len(self.envmap) <= 0:
+            raise RenderPassException('no value given to parameter \'envmap\'\ for ' + 
+                    PhotonPass.getTypeName() + ' pass')
+
+        out = [assetman.find(self.envmap)]
+        self.envmap = assetman.convertTextureName(self.envmap)
+
+        # resolve shader
+        out.extend(self.env.resolveAssets(assetman))
+        self.env.setAsset('envmap', self.envmap)
+        params = self.env.getParameters()
+        if 'envmap' not in params:
+            raise RenderPassException('no parameter \'envmap\' in shader: ' + 
+                    self.env.getShaderName())
+
+        self._resolvedAssetPaths = True
+        return out
+
+def build(**kwargs):
+    return PhotonPass(**kwargs)
