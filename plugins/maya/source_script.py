74a638d027c8d1366c7dc1089eef82c1ac61b9c8
Aaron Arch <devnull@localhost>
2013-03-08 13:39:48 -0600

    removed source_script.py

--- plugins/maya/source_script.py
+++
@@ -1,7 +0,0 @@
-from sourceModule import psource
-psource( 'Z:\\VM_Shared\\arch_shared\\RMS\\RMS_python\\cr_interface.py' )
-psource( 'Z:\\VM_Shared\\arch_shared\\RMS\\RMS_python\\cr_GUI.py' )
-psource( 'Z:\\VM_Shared\\arch_shared\\RMS\\RMS_python\\cr_RenderObject.py' )
-psource( 'Z:\\VM_Shared\\arch_shared\\RMS\\RMS_python\\cr_Simulation.py' )
-import AETemplates
-reload(AETemplates)
