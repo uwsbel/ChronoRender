c1f6e6fc8c99cb975296684351960c9ec9e1ea08
Aaron Arch <devnull@localhost>
2013-03-08 15:06:51 -0600

    added some method stubs for auto sim import in maya

--- plugins/maya/RMS/RMS_python/cr_for_maya/cr_Simulation.py
+++ plugins/maya/RMS/RMS_python/cr_for_maya/cr_Simulation.py
@@ -5,6 +5,7 @@ from cr_Object import CRObject, CRObject_Node
 from cr_RenderObject import CRRenderObject
 from cr_DataObject import CRDataObject
 
+from chronorender.finder import FinderFactory
 from chronorender.data import DataObject
 from chronorender.datasource import DataSource
 from chronorender.renderobject import RenderObject
@@ -56,16 +57,77 @@ class CRSimulation(CRObject):
         return CRSimulation_Node()
 
     def export(self, md):
+        sim = self._constructSim()
+        md.addElement(Simulation.getTypeName(), sim.getSerialized())
+        del sim
+
+    def _import(self):
+        sim = self._constructSim()
+        sim.resolveAssets(FinderFactory().build('.'))
+        lastframe = sim.getNumFrames()
+        transforms = {}
+        for frame in range(0, lastframe+1):
+            curr_id = 0
+
+            for robj in sim._robjs:
+                data = sim._data.getData(frame, robj.condition)
+
+                for entry in data:
+                    if curr_id not in transforms:
+                        transforms[curr_id] = pm.polyCube(n=self.node.name+str(frame))[0]
+
+                    CRSimulation._setMayaTrans(transforms, curr_id, entry)
+                    CRSimulation._setMayaRot(transforms, curr_id, entry)
+                    curr_id += 1
+
+            pm.setKeyframe(transforms.values(),at="translate",t=frame)
+            pm.setKeyframe(transforms.values(),at="rotate",t=frame)
+        del sim
+
+    def _importRobjs(self, frame, transforms):
+        curr_id = 0
+        if curr_id not in transforms:
+            transforms[curr_id] = pm.polyCube(n=self.node.name+str(i))[0]
+
+        transforms[curr_id].translate.set(curr_trans[0],curr_trans[1],curr_trans[2])
+        transforms[curr_id].rotate.set(rot[0],rot[1],rot[2])
+        curr_id += 1
+
+    @staticmethod
+    def _setMayaTrans(transforms, curr_id, entry):
+        curr_trans = [0.0, 0.0, 0.0]
+        if 'pos_x'in entry:
+            curr_trans[0] = entry['pos_x']
+        if 'pos_y'in entry:
+            curr_trans[0] = entry['pos_y']
+        if 'pos_z'in entry:
+            curr_trans[0] = entry['pos_z']
+        transforms[curr_id].translate.set(curr_trans[0],
+                curr_trans[1],curr_trans[2])
+
+    @staticmethod
+    def _setMayaRot(transforms, curr_id, entry):
+        curr_rot = [0.0, 0.0, 0.0]
+        if 'euler_x'in entry:
+            curr_rot[0] = entry['euler_x']
+        if 'euler_y'in entry:
+            curr_rot[0] = entry['euler_y']
+        if 'euler_z'in entry:
+            curr_rot[0] = entry['euler_z']
+        transforms[curr_id].rotate.set(curr_rot[0],
+                curr_rot[1],curr_rot[2])
+
+    def _constructSim(self):
         attrdict = self.attrs2Dict()
         simdict = attrdict[Simulation.getTypeName()]
         sim = self.sim_factories.build(Simulation.getTypeName(), **simdict)
-        md.addElement(Simulation.getTypeName(), sim.getSerialized())
-        del sim
+        return sim
 
     def createFormGUI(self):
         win = super(CRSimulation, self).createFormGUI()
         self._createDataGUI()
         self._createRObjGUI()
+        # self._createImportGUI()
 
     def _createDataGUI(self):
         pm.text( label='Data' ) 
@@ -90,3 +152,9 @@ class CRSimulation(CRObject):
                 c=pm.Callback(self.addChildEnumCB, CRRenderObject,
                     self.robjs,
                     srcattr=CRSimulation_Node._robjTypeAttr))
+
+    def _createImportGUI(self):
+        pm.text( label='Import' ) 
+        pm.text( label='This imports the entire simulation' ) 
+        pm.button(label="Import", w=128,
+                c=pm.Callback(self._import))
