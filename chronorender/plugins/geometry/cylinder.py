d78b6f7ac77fa80f3724132bbec3c345b7512310
Daniel Kaczmarek <dkaczmarek@wisc.edu>
2013-07-10 13:32:05 -0500

    cylinder now takes height as param instead of zmin/max

--- chronorender/plugins/geometry/cylinder.py
+++ chronorender/plugins/geometry/cylinder.py
@@ -9,16 +9,18 @@ class Cylinder(Geometry):
         super(Cylinder,self).__init__(*args, **kwargs)
 
         self.radius = self.getMember('radius')
-        self.zmin = self.getMember('zmin') #zmax-zmin = heigh
-        self.zmax = self.getMember('zmax')
+        self.height = self.getMember('height')
+        # self.zmin = self.getMember('zmin') #zmax-zmin = heigh
+        # self.zmax = self.getMember('zmax')
         self.thetamax = self.getMember('thetamax')
 
     def _initMembersDict(self):
         super(Cylinder,self)._initMembersDict()
 
         self._members['radius']     = [float, 1.0]
-        self._members['zmin']       = [float,-1.0]
-        self._members['zmax']       = [float, 1.0]
+        self._members['height']     = [float, 1.0]
+        # self._members['zmin']       = [float,-1.0]
+        # self._members['zmax']       = [float, 1.0]
         self._members['thetamax']   = [float, 360.0]
 
     # def updateMembers(self):
@@ -28,7 +30,7 @@ class Cylinder(Geometry):
     #     self.setMember('zmax', self.zmax)
 
     def render(self, rib, *args, **kwargs):
-        rib.Cylinder(self.radius, self.zmin, self.zmax, self.thetamax, *args, **self._params)
+        rib.Cylinder(self.radius, -self.height/2.0, self.height/2.0, self.thetamax, *args, **self._params)
 
 def build(**kwargs):
     return Cylinder(**kwargs)
